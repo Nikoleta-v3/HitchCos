6bc975ecc7f239c3e29ef88b7f78ff46c508c9d8
Shree7676 <shreekantnandiyawar@gmail.com>
2024-08-27 16:52:45 +0200

    bubble sort

--- src/listwiz/sorting.py
+++ src/listwiz/sorting.py
@@ -46,11 +46,20 @@ def merge_sort(l):
     return res
 
 
-def bubble_sort(l):
-    # We should provide bubble sort as well!
-    raise NotImplementedError
+def bubble_sort(arr):
+    n = len(arr)
+    # Traverse through all array elements
+    for i in range(n):
+        # Last i elements are already sorted
+        for j in range(0, n - i - 1):
+            # Traverse the array from 0 to n-i-1
+            # Swap if the element found is greater
+            # than the next element
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
 
 def selection_sort(l):
     # We should provide selection sort.
     raise NotImplementedError
+
