79640744db2bf12e58e0ea6fc94de1e455ff4d6c
Gert-Ludwig Ingold <gert.ingold@physik.uni-augsburg.de>
2024-08-29 16:17:57 +0200

    empty list returned without sorting (#25)
    This PR proposes to handle empty lists like lists of length 1 and to return the list unmodified. This solution should fix #8

--- tests/test_sorting.py
+++ tests/test_sorting.py
@@ -16,9 +16,10 @@ def test_mergesort():
 
 
 def test_mergesort_empty():
-    # Stub for a test with empty lists, see issue #8
-    pass
-
+    # Test with empty list:
+    l = []
+    res = lws.merge_sort(l)
+    assert res == []
 
 @pytest.mark.parametrize(
     "input_list, expected",
